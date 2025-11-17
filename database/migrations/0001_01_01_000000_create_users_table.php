eaba28670c8d43f38d41f664e765c74d7deffcda
yoga <yogaadipratama22@gmail.com>
2025-11-17 17:41:27 +0700

    Migrations Table Users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username')->unique()->fillable(); // username harus unik
+            $table->string('password')->nullable(); // password boleh kosong
+            $table->string('fullname'); 
+            $table->string('email')->unique()->fillable();
+            $table->string('phone');
+            $table->unsignedBigInteger('role_id'); // untuk relasi ke tabel roles
+            $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');  // relasi ke tabel roles
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
