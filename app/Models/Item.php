402402f770260c41cda6c1d67e1d08be03fe68bb
lordfroze <yogaadipratama22@gmail.com>
2025-11-18 18:48:28 +0700

    model sampai order

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
 
 class Item extends Model
 {
     //
+    use SoftDeletes, HasFactory; // menggunakan soft delete dan factory karena Item menggunakan factory ItemFactory.php
+
+    protected $fillable = ['name', 'description', 'price', 'category_id', 'img', 'is_active', 'created_at', 'updated_at'];
+    protected $dates = ['deleted_at'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class); // setiap item hanya memiliki 1 category
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    // disesuiakan dengan migration table orders
+    protected $fillable = ['order_code', 'user_id', 'subtotal', 'tax', 'grand_total','status', 'table_number', 'payment_method', 'note', 'created_at', 'updated_at'];
+    protected $dates = ['deleted_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class); // setiap order hanya memiliki 1 user
+    }
 }
