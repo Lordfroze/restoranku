f8509e2c08c47efe690647395d2f00a4bc4e988f
lordfroze <yogaadipratama22@gmail.com>
2025-11-18 17:10:07 +0700

    Migrations Table Orders and Order

---
+++ database/migrations/2025_11_18_164849_create_order_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order_id'); // untuk relasi ke tabel orders
+            $table->unsignedBigInteger('item_id'); // untuk relasi ke tabel items
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->integer('tax');
+            $table->integer('total_price');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('order_id')->references('id')->on('orders'); // membuat relasi ke tabel orders
+            $table->foreign('item_id')->references('id')->on('items'); // membuat relasi ke tabel items
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
