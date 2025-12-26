6d3c7300fe4ea2842db7211e241f49faceac24c4
yoga <yogaadipratama22@gmail.com>
2025-12-26 18:17:42 +0700

    Melihat Detail Pesanan

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\OrderItem;
 
 class OrderController extends Controller
 {
@@ -16,4 +17,13 @@ class OrderController extends Controller
         // Return the view with the orders
         return view('admin.order.index', compact('orders'));
     }
-}
+
+    public function show($id)
+    {
+        // Fetch the order by ID
+        $order = Order::findOrFail($id);
+        $orderItems = OrderItem::where('order_id', $order->id)->get(); // menampilkan order item yang berkaitan dengan order tersebut
+        // Return the view with the order details
+        return view('admin.order.show', compact('order', 'orderItems'));
+    }
+};
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     // disesuiakan dengan migration table orders
-    protected $fillable = ['order_code', 'user_id', 'subtotal', 'tax', 'grand_total','status', 'table_number', 'payment_method', 'note', 'created_at', 'updated_at'];
+    protected $fillable = ['order_code', 'user_id', 'subtotal', 'tax', 'grand_total','status', 'table_number', 'payment_method', 'notes', 'created_at', 'updated_at'];
     protected $dates = ['deleted_at'];
 
     public function user()
